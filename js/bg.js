caffdbddca6d8aa8e34a1ff2261c5ee90610fe67
kdd9512 <asdf@asdf.com>
2021-05-01 21:23:00 +0900

    todo 삭제 -> todos.js 로 변경.
    html 수정

--- js/bg.js
+++ js/bg.js
@@ -2,22 +2,33 @@ const body = document.querySelector("body");
 
 const IMG_NUM = 5
 
-
-function paintImage(imgNumber){
+function paintImage(imgNumber) {
   const image = new Image();
   image.src = `img/${imgNumber + 1}.jpg`;
-  image.classList.add("bgImage")
+  image.classList.add("bgImage");
   body.appendChild(image);
 }
 
-function genRandom(){
+function replaceChild() {
+  body.removeChild(body.querySelector("img"));
+  const num = genRandom();
+  const img = new Image();
+  img.src = `img/${num + 1}.jpg`;
+  img.classList.add("bgImage");
+  body.appendChild(img);
+}
+
+function genRandom() {
   const number = Math.floor(Math.random() * IMG_NUM);
   return number;
 }
 
-function init(){
+function init() {
   const randomNum = genRandom();
   paintImage(randomNum);
 }
+
 init();
-setInterval(init,5000);
+if (body.querySelector("img") !== null) {
+  setInterval(replaceChild, 5000);
+}
